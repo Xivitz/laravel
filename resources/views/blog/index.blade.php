1ff1a8fc999dd173cec3b274ac9df61b9d726855
Xivitz <diamatheus@gmail.com>
2017-12-28 18:08:45 -0200

    incluindo migrations e ajustes de layout

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -8,9 +8,9 @@
     @foreach($posts as $post)
     <div class="row">
         <div class="col-md-12 text-center">
-            <h1 class="post-title">{{ $post['title'] }}</h1>
-            <p>{{ $post['content'] }}</p>
-            <p><a href="{{ route('blog.post', ['id' => array_search($post, $posts)]) }}">Read More...</a></p>
+            <h1 class="post-title">{{ $post->title }}</h1>
+            <p>{{ $post->content }}</p>
+            <p><a href="{{ route('blog.post', ['id' => $post->id]) }}">Read More...</a></p>
         </div>
     </div>
     <hr>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Arizonia" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Orbitron" rel="stylesheet">
     <link rel="stylesheet" href="{{ URL::to('css/styles.css') }}">
 </head>
 @include('partials.header')
